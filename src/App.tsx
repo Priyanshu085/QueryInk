d1c9a9a0add06579ed80e43feba1d584b146e699
Priyanshu <abpriyanshu007@gmail.com>
2023-11-02 13:26:17 +0530

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
-import './App.css';
+import CodeDisplay from "./components/CodeDisplay";
+import MessageDisplay from "./components/MessageDisplay";
 
-function App() {
+const App = () => {
   return (
-    
+    <div className="app">
+      <div className="app">
+        <MessageDisplay />
+        <input/>
+        <CodeDisplay/>
+        <div className="button-container">
+        <button id="get-query">Get Query!</button>
+        <button id="clear-chat">Clear Chat</button>
+        </div>
+        </div>
+    </div>
   );
 }
 
---
+++ src/components/MessageDisplay.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MessageDisplay = () => {
+  return (
+    <div>MessageDisplay</div>
+  )
+}
+
+export default MessageDisplay
